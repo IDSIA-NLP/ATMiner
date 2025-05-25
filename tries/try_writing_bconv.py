cb3a688416fc5d5cb720bd8e5a72d6ae7d7e8c19
coree <coreehi@gmail.com>
2025-05-26 00:49:49 +0200

    remove examples

--- tries/try_writing_bconv.py
+++
@@ -1,8 +0,0 @@
-import bconv
-
-coll = bconv.load('../data/tmp/input/awqipQxjojnzAwukvMo3ZZv9r3KW-zookeys.687.13164.bioc.json', fmt='bioc_json', byte_offsets=False)
-
-
-with open("./test_byte-offset-true.bioc.json", 'w', encoding='utf8') as f:
-    #TODO: Might need more specification of the different output formats options
-    bconv.dump(coll, f, fmt="bioc_json", byte_offsets=True)
